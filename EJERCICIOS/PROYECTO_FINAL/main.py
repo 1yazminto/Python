2baffb12eacac3baa8beccc3f8cd2d54e411ff90
1yazminto <103079658+1yazminto@users.noreply.github.com>
2022-07-19 20:06:25 -0500

    Update main.py

--- EJERCICIOS/PROYECTO_FINAL/main.py
+++ EJERCICIOS/PROYECTO_FINAL/main.py
@@ -2,29 +2,29 @@
 Proyecto Básico de Python (El Ahorcado).
 Basado en el proyecto de: Kylie Ying (@kylieyying). 
 """
-import random
-import string
-from palabras import palabras
+import random # importa una serie de numeros aleatorios
+import string # importa una cadena
+from palabras import palabras #se define 
 from ahorcado_diagramas import vidas_diccionario_visual
 
 
-def obtener_palabra_válida(palabras):
+def obtener_palabra_válida(palabras):#para seleccionar al azar palabra 
     palabra = random.choice(palabras) 
 
 
-    while '-' in palabra or ' ' in palabra:
-        palabra = random.choice(palabras)
+    while '-' in palabra or ' ' in palabra:#se hace un ciclo hasta que se cumpla la condicion
+        palabra = random.choice(palabras)#buscar la palabra
 
-    return palabra.upper()
+    return palabra.upper()#palabra en mayusculas
 
 
-def ahorcado():
+def ahorcado():# define una funcion el nombre
 
-    print("=======================================")
+    print("=======================================")#hace caja para hacer la bienvenida al juego para mostrarla 
     print(" ¡Bienvenido(a) al juego del Ahorcado! ")
     print("=======================================")
 
-    palabra = obtener_palabra_válida(palabras)
+    palabra = obtener_palabra_válida(palabras)# definir una funcion con palabra, para hacer una lista de palabras
     letras_por_adivinar = set(palabra)  
     abecedario = set(string.ascii_uppercase) 
     letras_adivinadas = set()  
